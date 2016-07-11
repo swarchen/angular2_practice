eee5e7e6575b0334973a7aadc53041fc9da84ad8
jhaocheng <jhaocheng.wu@gmail.com>
2016-07-11 17:53:48 +0800

    add pagination

--- app/posts/posts.component.js
+++ app/posts/posts.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './posts.service', '../users/users.service', '../shared/spinner.component'], function(exports_1, context_1) {
+System.register(['angular2/core', './posts.service', '../users/users.service', '../shared/spinner.component', '../shared/pagination.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['angular2/core', './posts.service', '../users/users.service', '
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, posts_service_1, users_service_1, spinner_component_1;
+    var core_1, posts_service_1, users_service_1, spinner_component_1, pagination_component_1;
     var PostsComponent;
     return {
         setters:[
@@ -25,6 +25,9 @@ System.register(['angular2/core', './posts.service', '../users/users.service', '
             },
             function (spinner_component_1_1) {
                 spinner_component_1 = spinner_component_1_1;
+            },
+            function (pagination_component_1_1) {
+                pagination_component_1 = pagination_component_1_1;
             }],
         execute: function() {
             PostsComponent = (function () {
@@ -33,11 +36,22 @@ System.register(['angular2/core', './posts.service', '../users/users.service', '
                     this.usersService = usersService;
                     this.commentsLoading = true;
                     this.posts = [];
+                    this.pagedposts = [];
+                    this.currentPage = 1;
+                    this.pagesize = 10;
                     this.users = [];
                 }
                 PostsComponent.prototype.ngOnInit = function () {
                     this.loadPosts();
                     this.loadUsers();
+                };
+                PostsComponent.prototype.getPostsInPage = function (page) {
+                    var result = [];
+                    var startingIndex = (page - 1) * this.pagesize;
+                    var endIndex = Math.min(startingIndex + this.pagesize, this.posts.length);
+                    for (var i = startingIndex; i < endIndex; i++)
+                        result.push(this.posts[i]);
+                    return result;
                 };
                 PostsComponent.prototype.loadUsers = function () {
                     var _this = this;
@@ -50,29 +64,30 @@ System.register(['angular2/core', './posts.service', '../users/users.service', '
                     this.postsService.getPosts(filter)
                         .subscribe(function (res) {
                         _this.posts = res;
-                        _this.postsLoading = false;
-                    });
+                        _this.pagedposts = _this.getPostsInPage(1);
+                    }, null, function () { return _this.postsLoading = false; });
+                    ;
                 };
                 PostsComponent.prototype.onPostClick = function (post) {
                     var _this = this;
                     this.currentpost = post;
                     this.commentsLoading = true;
                     this.postsService.getPostComment(post)
-                        .subscribe(function (res) {
-                        _this.currentpost.comments = res;
-                        _this.commentsLoading = false;
-                    });
+                        .subscribe(function (res) { return _this.currentpost.comments = res; }, null, function () { return _this.commentsLoading = false; });
                 };
                 PostsComponent.prototype.reloadPosts = function (filter) {
                     this.currentpost = null;
                     this.loadPosts(filter);
+                };
+                PostsComponent.prototype.onPageChanged = function (page) {
+                    this.pagedposts = this.getPostsInPage(page);
                 };
                 PostsComponent = __decorate([
                     core_1.Component({
                         selector: 'posts',
                         templateUrl: 'app/posts/posts.template.html',
                         providers: [posts_service_1.PostsService, users_service_1.UsersService],
-                        directives: [spinner_component_1.SpinnerComponent],
+                        directives: [spinner_component_1.SpinnerComponent, pagination_component_1.PaginationComponent],
                         styles: ["\n\t\t.posts li { cursor: default; } \n\t\t.posts li:hover { background: #ecf0f1; }\n\t\t.list-group-item.active,   .list-group-item.active:hover,   .list-group-item.active:focus {  \n\t\tbackground-color: #ecf0f1;  border-color: #ecf0f1;   color: #2c3e50; \n\t\t}\n\t\timg {border-radius: 100%;}\n\t"]
                     }), 
                     __metadata('design:paramtypes', [posts_service_1.PostsService, users_service_1.UsersService])
--- app/posts/posts.component.ts
+++ app/posts/posts.component.ts
@@ -3,12 +3,13 @@ import {Component, OnInit} from 'angular2/core';
 import {PostsService} from './posts.service';
 import {UsersService} from '../users/users.service';
 import {SpinnerComponent} from '../shared/spinner.component';
+import {PaginationComponent} from '../shared/pagination.component';
 
 @Component({
 	selector: 'posts',
 	templateUrl: 'app/posts/posts.template.html',
 	providers: [PostsService, UsersService],
-	directives: [SpinnerComponent],
+	directives: [SpinnerComponent, PaginationComponent],
 	styles: [`
 		.posts li { cursor: default; } 
 		.posts li:hover { background: #ecf0f1; }
@@ -23,6 +24,9 @@ export class PostsComponent implements OnInit {
 	postsLoading;
 	commentsLoading = true;
 	posts = [];
+	pagedposts = [];
+	currentPage = 1;
+	pagesize = 10;
 	users = [];
 	currentpost;
 	constructor(
@@ -36,6 +40,16 @@ export class PostsComponent implements OnInit {
 		this.loadUsers();
 	}
 
+	private getPostsInPage(page){
+		var result = [];
+		var startingIndex = (page - 1) * this.pagesize;
+		var endIndex = Math.min(startingIndex + this.pagesize, this.posts.length);
+
+		for (var i = startingIndex; i < endIndex; i++)
+			result.push(this.posts[i]);
+		return result;
+	}
+
 	private loadUsers(){
 		this.usersService.getUsers()
 			.subscribe(res => this.users = res);
@@ -46,22 +60,29 @@ export class PostsComponent implements OnInit {
 		this.postsService.getPosts(filter)
 			.subscribe(res => {
 				this.posts = res;
-				this.postsLoading = false;
-			});
+				this.pagedposts = this.getPostsInPage(1);
+			},
+			null,
+			() => this.postsLoading = false;);
 	}
 
 	onPostClick(post) {
 		this.currentpost = post;
 		this.commentsLoading = true;
 		this.postsService.getPostComment(post)
-			.subscribe(res => {
-				this.currentpost.comments = res;
-				this.commentsLoading = false;
-			});
+			.subscribe(
+				res => this.currentpost.comments = res,
+				null,
+				() => this.commentsLoading = false
+			);
 	}
 
 	reloadPosts(filter){
 		this.currentpost = null;
 		this.loadPosts(filter);
+	}
+
+	onPageChanged(page){
+		this.pagedposts = this.getPostsInPage(page);
 	}
 }
